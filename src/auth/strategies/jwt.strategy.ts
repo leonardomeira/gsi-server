3324048c170748252a9144b8fd4af224d45c025f
Leonardo Meira <meiraleoabr@gmail.com>
2024-12-23 13:40:02 -0300

    fix(auth): fix wrong import at JwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,21 +2,21 @@ import { PassportStrategy } from "@nestjs/passport";
 import { Request } from "express";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { TokenPayload } from "../interfaces/token-payload.interface";
-import { UserService } from "src/user/user.service";
+import { UsersService } from "src/users/users.service";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private readonly userService: UserService) {
-        super({
-          jwtFromRequest: ExtractJwt.fromExtractors([
-            (request: Request) => request.cookies?.Authentication,
-          ]),
-          secretOrKey: process.env.JWT_AT_SECRET
-        });
-    }
+  constructor(private readonly usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies?.Authentication,
+      ]),
+      secretOrKey: process.env.JWT_AT_SECRET
+    });
+  }
 
-    validate(payload: TokenPayload) {
-        return this.userService.findOneById(payload.userId);
-    }
+  validate(payload: TokenPayload) {
+    return this.usersService.findOneById(payload.userId);
+  }
 }
